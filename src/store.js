5e346cc3f351c4eabe61f789c46ea0bbafe846ef
Jordan Ayala <Jordan Ayala>
2021-01-12 22:02:22 -0300

    Add redux logger

--- src/store.js
+++ src/store.js
@@ -1,19 +1,21 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+import logger from "redux-logger";
 
 const composeEnhancers = __DEV__
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // eslint-disable-line no-underscore-dangle
   : compose;
 
 export default initialState => {
-  const middlewares = [reduxImmutableStateInvariant(), thunk];
+  const middlewares = [reduxImmutableStateInvariant(), thunk, logger];
   const store = createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(...middlewares)),
+    composeEnhancers(applyMiddleware(...middlewares))
   );
 
-  return {store};
+  return { store };
 };
